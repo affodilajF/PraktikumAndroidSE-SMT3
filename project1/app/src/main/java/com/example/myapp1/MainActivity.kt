86f2075bd62968f0fc659f8868afa9d62578bb8d
Affodilaj Fadil <fadhilaainsalsabilla@mail.ugm.ac.id>
2023-08-30 04:42:39 -0700

    CHECK

---
+++ project1/app/src/main/java/com/example/myapp1/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.myapp1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        with(binding){
+            val buttonHitung = binding.btnCountBmi
+
+            buttonHitung.setOnClickListener {
+                try {
+                    val inputValuebb = textFieldBB.editText?.text.toString().toDouble()
+                    val inputValuetb = textFieldTB.editText?.text.toString().toDouble()
+
+                    val bmi = (inputValuebb / (inputValuetb * inputValuetb))
+
+                    textCountedBmi.text = bmi.toString()
+
+                } catch (e: NumberFormatException) {
+                    Toast.makeText(this@MainActivity, "Masukkan berat atau tinggi yang valid", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
+
+
+
+
+
+    }
+}
